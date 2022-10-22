f4f4dfa3e609ca713b779000b31cd88b7fcc9f96
Jim Kowalski <jmk672@psu.edu>
2022-10-22 13:49:07 -0400

    secured user routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,23 +4,24 @@ import { addUser, deleteUser, getUser, getUsers, updateUser } from '../controlle
 // mongoose model
 import User from '../models/userModel.js'
 import { createError } from '../utils/error.js'
+import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js'
 
 // router for exports
 const router = express.Router()
 
 // get all users
-router.get('/', getUsers)
+router.get('/', verifyToken, getUsers)
 
 // get a user
-router.get('/:id', getUser)
+router.get('/:id', verifyUser , getUser)
 
 // add a new user
-router.post('/', addUser)
+router.post('/', verifyAdmin, addUser)
 
 // update a user
-router.patch('/:id', updateUser)
+router.patch('/:id', verifyAdmin, updateUser)
 
 // remove a user
-router.delete('/:id', deleteUser)
+router.delete('/:id', verifyAdmin, deleteUser)
 
 export default router
