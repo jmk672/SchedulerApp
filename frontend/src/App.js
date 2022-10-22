2eefe444370c30cea96e3e11ad81fd5598c947c6
Jim Kowalski <jmk672@psu.edu>
2022-10-21 21:40:04 -0400

    more sidebar styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import Instructor from './pages/Instructor';
 import Admin from './pages/Admin';
@@ -16,26 +16,28 @@ function App() {
       <div className="App">
         <AuthProvider>
           <NavBar/>
-          <div className="row">
-            <div className="col-md-3 col-lg-2">
-              <Routes>
-                <Route path='/admin/*' element={ <AdminNav /> } />
-                <Route path='/instructor/*' element={ <InstructorNav /> } />
-              </Routes>
+            <div className="row">
+              <div className="col-md-3 col-lg-2 p-0 ps-2 border min-vh-100 ">
+                <Routes>
+                  <Route path='/admin/*' element={ <AdminNav /> } />
+                  <Route path='/instructor/*' element={ <InstructorNav /> } />
+                  <Route path='*' element={<div/>}/>
+                </Routes>
+              </div>
+              <div className="col-md-9 col-lg-10 border-top">
+                <Routes>
+                  <Route path="/admin/home" element={ <Admin />} />
+                  <Route path="/admin/addUser" element={ <h1>Add user</h1>} />
+                  <Route path="/admin/addExam" element={ <h1>Add new exam</h1>} />
+                  <Route path="/admin/schedule" element={ <h1>Schedule makeup</h1>} />
+                  <Route path="/instructor/home" element={ <Instructor/>} />
+                  <Route path="/login" element={<Login/>} />
+                  <Route exact path="/" element={<Login/>} />
+                  <Route path="/404/" element={<NotFound/>} />
+                  <Route path="*" element={<Navigate to="/404"/>} />
+                </Routes>
+              </div>
             </div>
-            <div className="col-md-9 col-lg-10">
-              <Routes>
-                <Route path="/admin/home" element={ <Admin />} />
-                <Route path="/admin/addUser" element={ <h1>Add user</h1>} />
-                <Route path="/admin/addExam" element={ <h1>Add new exam</h1>} />
-                <Route path="/admin/schedule" element={ <h1>Schedule makeup</h1>} />
-                <Route path="/instructor/home" element={ <Instructor/>} />
-                <Route path="/login" element={<Login/>} />
-                <Route exact path="/" element={<Login/>} />
-                <Route path="*" element={<NotFound/>} />
-              </Routes>
-            </div>
-          </div>
         </AuthProvider>
       </div>
     </BrowserRouter>
--- frontend/src/components/AdminNav.js
+++ frontend/src/components/AdminNav.js
@@ -16,7 +16,7 @@ const AdminNav = () => {
     return(
             <nav id="adminSidebar" className='d-lg-block sidebar'>
                 <div className="position-sticky">
-                    <div className='list-group list-group-flush mx-3 mt-4'>
+                    <div className='list-group list-group-flush m-0'>
                     <NavLink id="home" className='list-group-item list-group-item-action py-2'  to="/admin/home"><span>Admin Home</span></NavLink>
                     <NavLink id="addUser" className='list-group-item list-group-item-action py-2'  to="/admin/addUser"><span>Add User</span></NavLink>
                     <NavLink id="addExam" className='list-group-item list-group-item-action py-2'  to="/admin/addExam"><span>Add New Exam</span></NavLink>
