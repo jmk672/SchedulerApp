d97c41486482bad104dea8cc69b38d9a2aa27207
Jim Kowalski <jmk672@psu.edu>
2022-10-27 23:00:03 -0400

    finished batchHours controller

--- backend/controllers/hours.js
+++ backend/controllers/hours.js
@@ -21,6 +21,86 @@ export const setHours = async ( req, res, next ) => {
     }
 }
 
+export const batchHours = async (req, res, next) => {
+    const { sundayCode, sundayOpen, sundayClose,
+        mondayCode, mondayOpen, mondayClose,
+        tuesdayCode, tuesdayOpen, tuesdayClose,
+        wednesdayCode, wednesdayOpen, wednesdayClose,
+        thursdayCode, thursdayOpen, thursdayClose,
+        fridayCode, fridayOpen, fridayClose,
+        saturdayCode, saturdayOpen, saturdayClose} = req.body
+
+    try {
+        // clear week submitted
+        await Hours.findOneAndDelete({date: sundayCode})
+        await Hours.findOneAndDelete({date: mondayCode})
+        await Hours.findOneAndDelete({date: tuesdayCode})
+        await Hours.findOneAndDelete({date: wednesdayCode})
+        await Hours.findOneAndDelete({date: thursdayCode})
+        await Hours.findOneAndDelete({date: fridayCode})
+        await Hours.findOneAndDelete({date: saturdayCode})
+
+        if (sundayOpen && sundayClose) {
+            await Hours.create({
+                date: sundayCode,
+                startTime: sundayOpen,
+                endTime: sundayClose
+            })
+        }
+
+        if (mondayOpen && mondayClose) {
+            await Hours.create({
+                date: mondayCode,
+                startTime: mondayOpen,
+                endTime: mondayClose
+            })
+        }
+
+        if (tuesdayOpen && tuesdayClose) {
+            await Hours.create({
+                date: tuesdayCode,
+                startTime: tuesdayOpen,
+                endTime: tuesdayClose
+            })
+        }
+
+        if (wednesdayOpen && wednesdayClose) {
+            await Hours.create({
+                date: wednesdayCode,
+                startTime: wednesdayOpen,
+                endTime: wednesdayClose
+            })
+        }
+
+        if (thursdayOpen && thursdayClose) {
+            await Hours.create({
+                date: thursdayCode,
+                startTime: thursdayOpen,
+                endTime: thursdayClose
+            })
+        }
+
+        if (fridayOpen && fridayClose) {
+            await Hours.create({
+                date: fridayCode,
+                startTime: fridayOpen,
+                endTime: fridayClose
+            })
+        }
+
+        if (saturdayOpen && saturdayClose) {
+            await Hours.create({
+                date: saturdayCode,
+                startTime: saturdayOpen,
+                endTime: saturdayClose
+            })
+        }
+
+        res.status(200).json({message: "Update Successful"})
+    } catch (err) {
+        next(err)
+    }
+}
 export const deleteHours = async ( req, res, next ) => {
     const date = req.params.date
 
@@ -31,3 +111,4 @@ export const deleteHours = async ( req, res, next ) => {
         next(err)
     }
 }
+
